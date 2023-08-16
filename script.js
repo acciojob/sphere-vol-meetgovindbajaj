64e62038607e65d0d99d83dd4af4a8c22aa5e3f2
Govind Bajaj <meetgovindbajaj@gmail.com>
2023-08-16 21:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  //Write your code here
+  const rad = document.getElementById("radius").value;
+  const vol = document.getElementById("volume");
+  vol.value = ((4 / 3) * 3.14 * rad * rad * rad).toFixed(3);
+}
