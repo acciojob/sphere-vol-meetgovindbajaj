3f64b951f37cfebb2dc1a15feb30d55f8fd10f43
Govind Bajaj <meetgovindbajaj@gmail.com>
2023-08-16 21:22:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function volume_sphere() {
   //Write your code here
   const rad = document.getElementById("radius").value;
   const vol = document.getElementById("volume");
-  vol.value = ((4 / 3) * 3.1415928 * rad * rad * rad).toFixed(3);
+  vol.value = ((4 / 3) * 3.1415928 * rad * rad * rad).toFixed(4);
 }
